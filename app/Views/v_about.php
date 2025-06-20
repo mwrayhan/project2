8e1ec19a8ee890c66f7d80c7035cd580183370b5
Raihanul mawa <anjingtanah123.rm@gmail.com>
2025-06-20 11:31:09 +0700

    add file v_about

---
+++ app/Views/v_about.php
@@ -0,0 +1,46 @@
+<?= $this->extend('layouts/template-page'); ?>
+<?= $this->section('content'); ?>
+
+<!-- About Section -->
+</section><section id="about" class="about section">
+
+<div class="container section-title" data-aos="fade-up">
+  <h2>Tentang Saya</h2>
+  <p>Seorang mahasiswa IT yang memiliki ketertarikan mendalam pada dunia teknologi dan pemrograman. Di luar kegiatan akademis, saya juga adalah seorang penggemar traveling yang selalu antusias menjelajahi tempat-tempat baru. Perpaduan antara logika pemrograman dan petualangan ini menjadikan saya menjadi individu yang dinamis dan berpikiran terbuka.</p>
+</div><div class="container" data-aos="fade-up" data-aos-delay="100">
+
+  <div class="row gy-4 justify-content-center">
+    <div class="col-lg-4">
+      <img src="assets/img/fotoprofil.png" class="img-fluid" alt="Foto Profil Personal">
+    </div>
+    <div class="col-lg-8 content">
+      <h2>UI/UX Designer &amp; Web Developer.</h2>
+      <p class="fst-italic py-3">
+        Sebagai seorang UI/UX Designer dan Web Developer yang bersemangat, saya berdedikasi menciptakan pengalaman digital yang intuitif, menarik, dan fungsional. Dengan keahlian dalam desain antarmuka pengguna, pengalaman pengguna, serta pengembangan web *front-end* dan *back-end*, saya bertujuan untuk menerjemahkan ide-ide kompleks menjadi solusi web yang elegan dan efektif.
+      </p>
+      <div class="row">
+        <div class="col-lg-6">
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>09 Januari 2001</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span><a href="https://www.raihan.com" target="_blank">www.example.com</a></span></li> <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62 857-7351-7348</span></li> <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Cianjur, INA</span></li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>24</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor (Sedang Menempuh)</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span><a href="mailto:gusraihanulmawa@gmail.com">gusraihanulmawa@gmail.com</a></span></li> <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+          </ul>
+        </div>
+      </div>
+      <p class="py-3">
+        Saya percaya bahwa desain yang baik adalah jembatan antara teknologi dan manusia, dan saya selalu bersemangat untuk belajar serta mengaplikasikan inovasi terbaru dalam setiap proyek.
+      </p>
+    </div>
+  </div>
+
+</div>
+
+</section><!-- /Stats Section -->
+
+<?= $this->endSection(); ?>
